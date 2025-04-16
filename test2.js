401130a0072544a6f9aaada660c7752a851339e6
PavanSurisetti <pavan1898235@gmail.com>
2025-04-16 22:15:55 +0530

    Different types of Inputs

---
+++ test2.js
@@ -0,0 +1,7 @@
+let username;
+document.getElementById("Submit1").onclick=function(){
+    username=document.getElementById("Name").value;
+    console.log(username);
+    document.write(username);
+
+}
